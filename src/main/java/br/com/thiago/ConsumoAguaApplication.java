6fd3fc48e77800c022afd86a55f06f40c5c0566b
Thiago Ferrer <thi-ferrer@live.com>
2025-09-29 10:47:38 -0300

    fix: configurado custom start command no railway

--- src/main/java/br/com/thiago/ConsumoAguaApplication.java
+++ src/main/java/br/com/thiago/ConsumoAguaApplication.java
@@ -1,10 +1,12 @@
 package br.com.thiago;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+
 @EnableFeignClients  // <--- habilita Feign
 public class ConsumoAguaApplication {
 
