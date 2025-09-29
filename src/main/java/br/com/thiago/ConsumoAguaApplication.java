e09ab7e1fe45efe5b15d9fda5d72fd2249529583
Thiago Ferrer <thi-ferrer@live.com>
2025-09-29 11:43:02 -0300

    update - applications

--- src/main/java/br/com/thiago/ConsumoAguaApplication.java
+++ src/main/java/br/com/thiago/ConsumoAguaApplication.java
@@ -3,11 +3,12 @@ package br.com.thiago;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
-
-@EnableFeignClients  // <--- habilita Feign
+@EnableFeignClients  // habilita Feign
+@SpringBootApplication
 public class ConsumoAguaApplication {
 
     public static void main(String[] args) {
